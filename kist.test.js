41c44f77bac8a02f0fb92a08114f5bfd1d71f4ce
Blaise Ishimwe <bishimwe84@gmail.com>
2022-08-31 22:32:09 -0400

    edited kist testing suite

--- kist.test.js
+++ kist.test.js
@@ -1,11 +1,24 @@
+// module "C:/Users/bishi/Desktop/MIT Full Stack MERN/Week 7 Intro GitHub Testing and DOM/mitfacts/node_modules/course-utilities/index"
+
 var utils = require("course-utilities");
 
 //Define kist variable
 
 var kist = utils.load("./kist.js", "kist");
 
+// use describe method to group several related tests
+
 describe("KIST", () => {
   test("Object properties present", () => {
     expect(kist).toHaveProperty("student");
+  });
+
+  test("student", () => {
+    expect(kist.student).toBe("Blaise Ishimwe");
+  });
+
+  test("Founded range", () => {
+    expect(kist.founded).toBeLessThanOrEqual(2022);
+    expect(kist.founded).toBeGreaterThanOrEqual(1990);
   });
 });
